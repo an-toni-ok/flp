75741552fb0e37a94675872a348ad078318bbca3
F. Antonio Klemm <antonio@klemm.biz>
2024-06-21 21:31:38 +0200

    Moved configuration data to the file config.py, added an extensions file to have a single global redis instance (so that in case of changes to the ip address only one thing needs to updated). Added session token handling.

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -1,5 +1,7 @@
 from celery import Celery, Task
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, session
+
+from server.extensions import init_redis
 
 def celery_init_app(app: Flask) -> Celery:
     """Initializes Celery based of the flask app instance.
@@ -37,18 +39,58 @@ def create_app() -> Flask:
     """
     app = Flask(__name__)
     app.config.from_mapping(
-        CELERY=dict(
-            broker_url="redis://redis",
-            result_backend="redis://redis",
-            task_ignore_result=True,
-        ),
+        SECRET_KEY=b'_5#y2L"F4Q8z\n\xec]/'
+    #     CELERY=dict(
+    #         broker_url="redis://redis",
+    #         result_backend="redis://redis",
+    #         task_ignore_result=True,
+    #     ),
     )
+
+    from server.config import Config
+    app.config.from_object(Config)
+
     app.config.from_prefixed_env()
     celery_init_app(app)
+    init_redis(app)
 
     @app.errorhandler(400)
     def invalid_request_data(e):
         return jsonify(error=str(e)), 400
+    
+    @app.before_request
+    def assign_session_token():
+        from server.redis_util import ensure_data_settable
+        from server.extensions import redis_client
+
+        if "id" in session:
+            try:
+                # The tokens are in an integer format, if the conversion
+                # to int throws an error the token isn't valid
+                session_id = str(int(session['id']))
+                
+                if not bool(redis_client.sismember("session_ids", session_id)):
+                    # Token not yet saved, but valid
+                    redis_client.sadd("session_ids", session_id)
+                ensure_data_settable(session_id)
+                return
+            except:
+                # Invalid token
+                pass
+
+        # Generate a new token
+        counter = 0
+        while True:
+            session_id = str(hash(request.origin + str(counter)))
+            if not bool(redis_client.sismember("session_ids", session_id)):
+                # Store the session_id in redis before the session, so
+                # that the same id cannot be used twice because of
+                # network errors when writing it to redis. 
+                redis_client.sadd("session_ids", session_id)
+                ensure_data_settable(session_id)
+                session["id"] = session_id
+                break
+            counter += 1
 
     from server.routes import routes
 
---
+++ backend/server/extensions.py
@@ -0,0 +1,13 @@
+from redis import Redis
+
+redis_client = None
+
+def init_redis(app):
+    global redis_client
+    redis_client = Redis(
+        host=app.config['REDIS_HOST'],
+        port=app.config['REDIS_PORT'],
+        db=app.config['REDIS_DB'],
+        password=app.config['REDIS_PASSWORD'],
+        decode_responses=True
+    )
