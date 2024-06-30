262204f9b90d721be30338cc5aa004396981df93
F. Antonio Klemm <antonio@klemm.biz>
2024-07-01 01:01:05 +0200

    The logger is now usable from celery as well.

--- backend/server/extensions.py
+++ backend/server/extensions.py
@@ -42,6 +42,7 @@ def celery_init_app(app: Flask) -> Celery:
     celery_app.config_from_object(app.config["CELERY"])
     celery_app.set_default()
     app.extensions["celery"] = celery_app
+    setup_logger(app)
     return celery_app
 
 def setup_logger(app: Flask):
