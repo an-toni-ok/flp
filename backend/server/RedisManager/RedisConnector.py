57bf028d5ed6c319d226cdb9dadaa5680b15bcd3
F. Antonio Klemm <antonio@klemm.biz>
2024-07-04 00:13:57 +0200

    Added an abstraction for the Redis Access encapsulating every Redis access. Added tests for its functionality.

---
+++ backend/server/RedisManager/RedisConnector.py
@@ -0,0 +1,136 @@
+from enum import Enum
+from typing import Dict
+
+from redis import Redis
+
+from server.RedisManager.util import RunStatus
+from server.extensions import redis
+
+class RedisInput(Enum):
+    ALL = "all"
+    AREAS = "areas"
+    R_AREAS = "restricted_areas"
+    TECHNOLOGIES = "technologies"
+    STEPS = "production_steps"
+    MACHINES = "machines"
+    OBJECTIVES = "objectives"
+    CYCLE_TIME = "target_cycle_time"
+    OPERATOR_COST = "hourly_operator_cost"
+
+    def query(self, run_id):
+        global redis
+
+        return redis.json().get(run_id, str(self))[0]
+    
+    def set(self, run_id, data):
+        global redis
+ 
+        redis.json().set(run_id, str(self), data)
+    
+    def __str__(self) -> str:
+        if self.value == RedisInput.ALL.value:
+            return f"$.input"
+        return f"$.input.{self.value}"
+    
+class RedisRunConfig(Enum):
+    ALL = "all"
+    OUTPUT = "output"
+    EXECUTION_ID = "execution_id"
+    STATUS = "status"
+
+    def query(self, run_id):
+        global redis
+ 
+        return redis.json().get(run_id, str(self))[0]
+    
+    def set(self, run_id, data: Dict):
+        global redis
+
+        try:
+            run_input = redis.json().get(run_id)["input"]
+        except:
+            raise Exception("Run is not created")
+
+        if self.value == RedisRunConfig.ALL.value:
+            if data.get("input") is None:
+                # Set the input data  to the currently set state 
+                # so that the rest of the dict can be updated with 
+                # the input data remaining unchanged
+                data["input"] = run_input
+            elif data["input"] != run_input:
+                raise Exception("Invalid method for changing input data.")
+
+        redis.json().set(run_id, str(self), data)
+
+    def create(run_id):
+        global redis
+ 
+        if redis.json().get(run_id, "$"):
+            # do not allow overwriting existing data
+            raise Exception("Run id already set.")
+
+        redis.json().set(run_id, "$", {
+            'input': {
+                'areas': {}, 
+                'restricted_areas': {}, 
+                'technologies': [], 
+                'production_steps': {}, 
+                'machines': {}, 
+                'objectives': {}, 
+                'target_cycle_time': None, 
+                'hourly_operator_cost': None
+            }, 
+            'output': {}, 
+            'execution_id': None, 
+            'status': RunStatus.INPUT.value
+        })
+    
+    def __str__(self) -> str:
+        if self.value == RedisRunConfig.ALL.value:
+            return f"$"
+        return f"$.{self.value}"
+
+class RedisSession:
+    def contains(session_id):
+        global redis
+ 
+        return bool(redis.sismember("session_ids", session_id))
+
+    def create_session(session_id):
+        global redis
+ 
+        redis.sadd("session_ids", session_id)
+        redis.json().set(session_id, "$", {
+            "current_run_nr": 0,
+            "run_ids": []
+        })
+
+    def create_run(session_id) -> int:
+        """Creates a new run in the redis session data storage.
+
+        Args:
+            session_id (_type_): The session id where the run is created.
+
+        Raises:
+            Exception: Thrown if the Session isn't set.
+
+        Returns:
+            int: The run_id of the new run.
+        """
+        global redis
+
+        try:
+            run_nr = redis.json().get(session_id, "$.current_run_nr")[0]
+            run_id = f"{session_id}_{run_nr}"
+        except TypeError:
+            raise Exception(f"Session { session_id } not set.")
+        
+        redis.json().set(session_id, "$.current_run_nr", run_nr + 1)
+        redis.json().arrappend(session_id, "$.run_ids", run_id)
+        return run_id
+
+class RedisIdCounter:
+    def incr(redis_client: Redis=None):
+        global redis
+
+        return redis.incr("session_id_counter")
---
+++ backend/tests/test_redis_connector.py
@@ -0,0 +1,493 @@
+import pytest
+
+from redis import Redis
+
+from server.RedisManagerOld.util import RunStatus
+from server.RedisManager.RedisConnector import RedisInput, RedisRunConfig, RedisSession, RedisIdCounter
+
+class TestRedisIdCounter:
+    def unset_session_id_counter(self, redis: Redis):
+        redis.delete("session_id_counter")
+
+    def test_counter_auto_init(self, redis: Redis):
+        self.unset_session_id_counter(redis)
+
+        RedisIdCounter.incr()
+        assert int(redis.get("session_id_counter")) == 1
+
+    def test_counter_incr_works(self, redis):
+        self.unset_session_id_counter(redis)
+        
+        value1 = RedisIdCounter.incr() # Init counter
+        value2 = RedisIdCounter.incr()
+        assert value2 == value1 + 1
+
+class TestRedisSession:
+    def unset_session_ids(self, redis: Redis):
+        redis.delete("session_ids")
+
+    def test_session_id_check_against_empty_session_ids(self, redis: Redis):
+        self.unset_session_ids(redis)
+
+        assert bool(RedisSession.contains("test")) == False
+
+    def test_session_creation(self, redis: Redis):
+        self.unset_session_ids(redis)
+
+        session_id = "test"
+        RedisSession.create_session(session_id)
+        assert session_id in list(redis.smembers("session_ids"))
+        assert redis.json().get(session_id) is not None
+
+    def test_session_id_check_after_creation(self, redis: Redis):
+        self.unset_session_ids(redis)
+
+        session_id = "test"
+        assert RedisSession.contains(session_id) == False
+        RedisSession.create_session(session_id)
+        assert RedisSession.contains(session_id) == True
+
+    def test_session_run_counter_incr(self, redis: Redis):
+        self.unset_session_ids(redis)
+
+        session_id = "test"
+        RedisSession.create_session(session_id)
+        run_nr = redis.json().get(session_id, "$.current_run_nr")[0]
+
+        RedisSession.create_run(session_id)
+        new_run_nr = redis.json().get(session_id, "$.current_run_nr")[0]
+        assert new_run_nr == run_nr + 1
+
+    def test_session_run_creation(self, redis: Redis):
+        self.unset_session_ids(redis)
+
+        session_id = "test"
+        RedisSession.create_session(session_id)
+        run_id = RedisSession.create_run(session_id)
+        stored_run_ids = redis.json().get(session_id, "$.run_ids")[0]
+        assert run_id in stored_run_ids
+
+    def test_creating_run_without_session_existing_raises_error(self, redis: Redis):
+        session_id = "test_session_id"
+        redis.delete(session_id) # Make sure it doesn't exist
+        with pytest.raises(Exception, match=f"Session { session_id } not set."):
+            RedisSession.create_run(session_id)
+
+class TestRedisRunConfig:
+    def reset_test_run(self, redis: Redis, run_id="test_run", delete_only=False):
+        redis.delete(run_id)
+        if delete_only:
+            return
+        RedisRunConfig.create(run_id)
+
+    def get_set_comparison_data(self, field: RedisRunConfig, field_value, redis: Redis):
+        run_id = "test_run"
+        self.reset_test_run(redis, run_id=run_id)
+
+        field.set(run_id, field_value)
+        redis_run_data = redis.json().get(run_id)
+        real_data = None
+        match field:
+            case RedisRunConfig.ALL:
+                real_data = redis_run_data
+            case RedisRunConfig.OUTPUT:
+                real_data = redis_run_data["output"]
+            case RedisRunConfig.EXECUTION_ID:
+                real_data = redis_run_data["execution_id"]
+            case RedisRunConfig.STATUS:
+                real_data = redis_run_data["status"]
+        return { "expected": field_value, "real": real_data }
+
+    def set_and_get_query_comparison_data(self, field: RedisRunConfig, field_value, redis: Redis):
+        run_id = "test_run"
+        self.reset_test_run(redis, run_id=run_id)
+
+        field.set(run_id, field_value)
+        real_data = field.query(run_id)
+        return { "expected": field_value, "real": real_data }
+
+    def test_created_run_has_correct_skeleton(self, redis: Redis):
+        run_id = "test_run"
+        self.reset_test_run(redis, run_id=run_id, delete_only=True)
+        init_data = {
+            'input': {
+                'areas': {}, 
+                'restricted_areas': {}, 
+                'technologies': [], 
+                'production_steps': {}, 
+                'machines': {}, 
+                'objectives': {}, 
+                'target_cycle_time': None, 
+                'hourly_operator_cost': None
+            }, 
+            'output': {}, 
+            'execution_id': None, 
+            'status': RunStatus.INPUT.value
+        }
+
+        RedisRunConfig.create(run_id)
+        test_run = redis.json().get(run_id)
+        assert test_run == init_data
+
+    def test_creation_of_existing_run_raises_error(self, redis: Redis):
+        run_id = "test_run"
+        self.reset_test_run(redis, run_id=run_id, delete_only=True)
+
+        RedisRunConfig.create(run_id)
+        with pytest.raises(Exception, match="Run id already set."):
+            RedisRunConfig.create(run_id)
+
+    def test_setting_option_of_non_existant_run_raises_error(self, redis: Redis):
+        run_id = "test_run"
+        self.reset_test_run(redis, run_id=run_id, delete_only=True)
+        test_data = {
+            'output': {
+                "test": "test"
+            }, 
+            'execution_id': 1, 
+            'status': RunStatus.COMPLETED.value
+        }
+
+        with pytest.raises(Exception, match="Run is not created"):
+            RedisRunConfig.ALL.set(run_id, test_data)
+
+    def test_setting_all_option_with_new_input_raises_error(self, redis: Redis):
+        run_id = "test_run"
+        self.reset_test_run(redis, run_id=run_id, delete_only=True)
+        test_data = {
+            'output': {
+                "test": "test"
+            }, 
+            'execution_id': 1, 
+            'status': RunStatus.COMPLETED.value,
+            'input': {"areas": "test"}
+        }
+
+        RedisRunConfig.create(run_id)
+        with pytest.raises(Exception, match="Invalid method for changing input data."):
+            RedisRunConfig.ALL.set(run_id, test_data)
+
+        redis_test_data = redis.json().get(run_id)
+        assert redis_test_data["input"] != test_data["input"]
+
+    def test_setting_all_option(self, redis: Redis):
+        test_data = {
+            'output': {
+                "test": "test"
+            }, 
+            'execution_id': 1, 
+            'status': RunStatus.COMPLETED.value
+        }
+        comparison_data = self.get_set_comparison_data(
+            RedisRunConfig.ALL, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_setting_output_option(self, redis: Redis):
+        test_output = {
+            "test": "test"
+        }
+        comparison_data = self.get_set_comparison_data(
+            RedisRunConfig.STATUS, 
+            test_output,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_setting_execution_id_option(self, redis: Redis):
+        comparison_data = self.get_set_comparison_data(
+            RedisRunConfig.EXECUTION_ID, 
+            1,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_setting_status_option(self, redis: Redis):
+        comparison_data = self.get_set_comparison_data(
+            RedisRunConfig.STATUS, 
+            RunStatus.COMPLETED.value,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_query_all_option(self, redis: Redis):
+        test_data = {
+            'output': {
+                "test": "test"
+            }, 
+            'execution_id': 1, 
+            'status': RunStatus.COMPLETED.value
+        }
+        comparison_data = self.set_and_get_query_comparison_data(
+            RedisRunConfig.ALL, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_query_output_option(self, redis: Redis):
+        test_output = {
+            "test": "test"
+        }
+        comparison_data = self.set_and_get_query_comparison_data(
+            RedisRunConfig.STATUS, 
+            test_output,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_query_execution_id_option(self, redis: Redis):
+        comparison_data = self.set_and_get_query_comparison_data(
+            RedisRunConfig.EXECUTION_ID, 
+            1,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_query_status_option(self, redis: Redis):
+        comparison_data = self.set_and_get_query_comparison_data(
+            RedisRunConfig.STATUS, 
+            RunStatus.COMPLETED.value,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+class TestRedisInput:
+    def reset_test_run_input(self, redis: Redis, run_id="test_run"):
+        try:
+            RedisRunConfig.create(run_id)
+        except Exception as e:
+            if str(e) != "Run id already set.":
+                raise Exception("Error during run creation")
+        redis.json().set(run_id, "$.input", {
+            'areas': {}, 
+            'restricted_areas': {}, 
+            'technologies': [], 
+            'production_steps': {}, 
+            'machines': {}, 
+            'objectives': {}, 
+            'target_cycle_time': None, 
+            'hourly_operator_cost': None
+        })
+
+    def get_set_comparison_data(self, field: RedisInput, field_value, redis: Redis):
+        run_id = "test_run"
+        self.reset_test_run_input(redis, run_id=run_id)
+
+        field.set(run_id, field_value)
+        redis_run_data = redis.json().get(run_id)["input"]
+        real_data = None
+        match field:
+            case RedisInput.ALL:
+                real_data = redis_run_data
+            case RedisInput.AREAS:
+                real_data = redis_run_data["areas"]
+            case RedisInput.R_AREAS:
+                real_data = redis_run_data["restricted_areas"]
+            case RedisInput.TECHNOLOGIES:
+                real_data = redis_run_data["technologies"]
+            case RedisInput.STEPS:
+                real_data = redis_run_data["production_steps"]
+            case RedisInput.MACHINES:
+                real_data = redis_run_data["machines"]
+            case RedisInput.OBJECTIVES:
+                real_data = redis_run_data["objectives"]
+            case RedisInput.CYCLE_TIME:
+                real_data = redis_run_data["target_cycle_time"]
+            case RedisInput.OPERATOR_COST:
+                real_data = redis_run_data["hourly_operator_cost"]
+        return { "expected": field_value, "real": real_data }
+
+    def set_and_get_query_comparison_data(self, field: RedisInput, field_value, redis: Redis):
+        run_id = "test_run"
+        self.reset_test_run_input(redis, run_id=run_id)
+
+        field.set(run_id, field_value)
+        real_data = field.query(run_id)
+        return { "expected": field_value, "real": real_data }
+    
+    # special: 
+    # technologies: array
+    # cycle_time + operator_cost: int
+
+    def test_setting_all_option(self, redis: Redis):
+        test_data = {
+            'areas': { "test": "test" }, 
+            'restricted_areas': { "test": "test" }, 
+            'technologies': [ "test", "test", "test" ], 
+            'production_steps': { "test": "test" }, 
+            'machines': { "test": "test" }, 
+            'objectives': { "test": "test" }, 
+            'target_cycle_time': 1, 
+            'hourly_operator_cost': 1
+        }
+        comparison_data = self.get_set_comparison_data(
+            RedisInput.ALL, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_setting_areas_option(self, redis: Redis):
+        test_data = { "test": "test" }
+        comparison_data = self.get_set_comparison_data(
+            RedisInput.AREAS, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_setting_r_areas_option(self, redis: Redis):
+        test_data = { "test": "test" }
+        comparison_data = self.get_set_comparison_data(
+            RedisInput.R_AREAS, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_setting_technologies_option(self, redis: Redis):
+        test_data = [ "test", "test", "test" ]
+        comparison_data = self.get_set_comparison_data(
+            RedisInput.TECHNOLOGIES, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_setting_steps_option(self, redis: Redis):
+        test_data = { "test": "test" }
+        comparison_data = self.get_set_comparison_data(
+            RedisInput.STEPS, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_setting_machines_option(self, redis: Redis):
+        test_data = { "test": "test" }
+        comparison_data = self.get_set_comparison_data(
+            RedisInput.MACHINES, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_setting_objectives_option(self, redis: Redis):
+        test_data = { "test": "test" }
+        comparison_data = self.get_set_comparison_data(
+            RedisInput.OBJECTIVES, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_setting_cycle_time_option(self, redis: Redis):
+        test_data = 1
+        comparison_data = self.get_set_comparison_data(
+            RedisInput.CYCLE_TIME, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_setting_operator_cost_option(self, redis: Redis):
+        test_data = 1
+        comparison_data = self.get_set_comparison_data(
+            RedisInput.OPERATOR_COST, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_query_all_option(self, redis: Redis):
+        test_data = {
+            'areas': { "test": "test" }, 
+            'restricted_areas': { "test": "test" }, 
+            'technologies': [ "test", "test", "test" ], 
+            'production_steps': { "test": "test" }, 
+            'machines': { "test": "test" }, 
+            'objectives': { "test": "test" }, 
+            'target_cycle_time': 1, 
+            'hourly_operator_cost': 1
+        }
+        comparison_data = self.set_and_get_query_comparison_data(
+            RedisInput.ALL, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_query_areas_option(self, redis: Redis):
+        test_data = { "test": "test" }
+        comparison_data = self.set_and_get_query_comparison_data(
+            RedisInput.AREAS, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_query_r_areas_option(self, redis: Redis):
+        test_data = { "test": "test" }
+        comparison_data = self.set_and_get_query_comparison_data(
+            RedisInput.R_AREAS, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_query_technologies_option(self, redis: Redis):
+        test_data = [ "test", "test", "test" ]
+        comparison_data = self.set_and_get_query_comparison_data(
+            RedisInput.TECHNOLOGIES, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_query_steps_option(self, redis: Redis):
+        test_data = { "test": "test" }
+        comparison_data = self.set_and_get_query_comparison_data(
+            RedisInput.STEPS, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_query_machines_option(self, redis: Redis):
+        test_data = { "test": "test" }
+        comparison_data = self.set_and_get_query_comparison_data(
+            RedisInput.MACHINES, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_query_objectives_option(self, redis: Redis):
+        test_data = { "test": "test" }
+        comparison_data = self.set_and_get_query_comparison_data(
+            RedisInput.OBJECTIVES, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_query_cycle_time_option(self, redis: Redis):
+        test_data = 1
+        comparison_data = self.set_and_get_query_comparison_data(
+            RedisInput.CYCLE_TIME, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
+
+    def test_query_operator_cost_option(self, redis: Redis):
+        test_data = 1
+        comparison_data = self.set_and_get_query_comparison_data(
+            RedisInput.OPERATOR_COST, 
+            test_data,
+            redis
+        )
+        assert comparison_data["expected"] == comparison_data["real"]
