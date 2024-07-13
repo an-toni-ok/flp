f18272fe38ee9e48ec7c509f5a2b2503e6bf747d
F. Antonio Klemm <antonio@klemm.biz>
2024-07-13 17:57:11 +0200

    Added a button that can take an svg via a slot and a help text (for accessibility).

---
+++ frontend/src/components/Inputs/IconButton.vue
@@ -0,0 +1,39 @@
+<script setup>
+const props = defineProps({
+    help_text: {
+        type: String,
+        required: true
+    }
+})
+
+defineEmits(['click', 'focus', 'blur']);
+</script>
+
+<template>
+    <button
+        @click="$emit('click')"
+        @focus="$emit('focus')" 
+        @blur="$emit('blur')"
+        class="input-button" 
+        :aria-label="help_text" >
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+.input-button {
+    border-left: none;
+    width: 30px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid var(--color-border);
+    box-sizing: border-box;
+    background-color: var(--color-background);
+}
+
+.input-button > svg {
+    width: 15px;
+}
+</style>
