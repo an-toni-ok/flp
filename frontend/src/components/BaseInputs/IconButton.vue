4a4683c4a2f73d93bac7f338b88bb198b7ef915b
F. Antonio Klemm <antonio@klemm.biz>
2024-07-14 14:25:34 +0200

    Added base components for text and number inputs and icon buttons in a special folder. They can be combined to create more complex inputs.

---
+++ frontend/src/components/BaseInputs/IconButton.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    help_text: {
+        type: String,
+        required: true
+    },
+    isFocused: {
+        type: Boolean,
+        default: false
+    },
+    hasBorderTop: {
+        type: Boolean,
+        default: true
+    },
+    hasBorderLeft: {
+        type: Boolean,
+        default: false
+    }
+})
+
+defineEmits(['click', 'focus', 'blur']);
+</script>
+
+<template>
+    <button
+        @click="$emit('click')"
+        class="icon-button" 
+        :class="{ 
+            'focus': isFocused, 
+            'no-top': !hasBorderTop, 
+            'no-left': !hasBorderLeft 
+        }"
+        :aria-label="help_text" >
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+/** 
+The base styling for HTML components like 
+buttons is in src/assets/main.css. 
+*/
+.icon-button {
+    width: 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.icon-button > * {
+    width: 15px;
+    height: 15px;
+}
+
+.no-top {
+    border-top: none;
+}
+
+.no-left {
+    border-left: none;
+}
+</style>
---
+++ frontend/src/components/BaseInputs/InputText.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps({
+    name: {
+        type: String,
+        required: true
+    },
+    id: {
+        type: String,
+        required: true
+    },
+    isFocused: Boolean
+})
+
+defineEmits(['focus', 'blur'])
+
+// Model: https://vuejs.org/guide/components/v-model.html
+const input_value = defineModel('value', { required: true }) 
+</script>
+
+<template>
+    <input 
+        @focus="$emit('focus')" 
+        @blur="$emit('blur')" 
+        class="text-input"
+        :class="{'focus': isFocused}"
+        type="text" 
+        :name="name" 
+        :id="id"
+        v-model="input_value" >
+</template>
+
+<style scoped>
+/** 
+The base styling for HTML components like 
+inputs is in src/assets/main.css. 
+*/
+</style>
