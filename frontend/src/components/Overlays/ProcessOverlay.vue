d00beacd7d1cd3e299f53bc41062c0152351f676
F. Antonio Klemm <antonio@klemm.biz>
2024-07-15 18:07:31 +0200

    Added a specific overlay for the process subpage.

---
+++ frontend/src/components/Overlays/ProcessOverlay.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue';
+
+import NumberInput from '../Inputs/NumberInput.vue';
+import TechnologyDropdownInput from '../Inputs/TechnologyDropdownInput.vue';
+import OverlayBase from './OverlayBase.vue';
+
+const opened = ref(true)
+const technology_value = ref("")
+const machine_time = ref(0)
+const manual_time = ref(0)
+</script>
+
+<template>
+    <OverlayBase title="Prozess erstellen">
+        <div class="overlay-content" v-show="opened">
+            <NumberInput 
+                name="Maschinenzeit"
+                id="machinenzeit"
+                v-model:value="machine_time" />
+            <NumberInput 
+                name="Manuelle Zeit"
+                id="manual-time"
+                v-model:value="manual_time" />
+            <TechnologyDropdownInput 
+                v-model:value="technology_value"
+                :is-changeable="true"
+                :is-deletable="true" />
+        </div>
+    </OverlayBase>
+</template>
+
+<style scoped>
+.overlay-content {
+    margin: 2rem 0;
+}
+.overlay-content > * {
+    margin: 1rem 0;
+}
+</style>
