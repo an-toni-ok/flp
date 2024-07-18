9fc4745d0946199fa861b1acaa6f2800c564acfe
F. Antonio Klemm <antonio@klemm.biz>
2024-07-18 03:49:08 +0200

    Created a button for completing the edit/add of something in an overlay.

---
+++ frontend/src/components/Buttons/OverlayButton.vue
@@ -0,0 +1,42 @@
+<script setup>
+const props = defineProps({
+    text: {
+        type: String,
+        required: true
+    }
+})
+
+defineEmits(['click']);
+</script>
+
+<template>
+    <button
+        @click="$emit('click')"
+        class="icon-button" >
+        {{ text }}
+    </button>
+</template>
+
+<style scoped>
+/** 
+The base styling for HTML components like 
+buttons is in src/assets/main.css. 
+*/
+.icon-button {
+    width: var(--input-width);
+    height: var(--input-height);
+    font-size: var(--font-size);
+    font-weight: 600;
+    padding: 0 calc((var(--input-height) - var(--font-size)) / 2);
+    border: 2px solid var(--color-border);
+    background-color: var(--color-background);
+}
+
+.icon-button:focus > * {
+    fill: var(--color-brand);
+}
+
+.icon-button > * {
+    width: 1rem;
+}
+</style>
