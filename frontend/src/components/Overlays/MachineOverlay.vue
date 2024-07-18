ebf6b703a8b973c7abebd7a92d219ba88440005c
F. Antonio Klemm <antonio@klemm.biz>
2024-07-18 04:02:14 +0200

    Added the button to the machine over and fixed a bug in it's display (the multi-input overflowing).

--- frontend/src/components/Overlays/MachineOverlay.vue
+++ frontend/src/components/Overlays/MachineOverlay.vue
@@ -1,11 +1,20 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+import { useTechnologiesStore } from '@/stores/technologies';
 
 import NumberInput from '../Inputs/NumberInput.vue';
 import MachineTypeDropdownInput from '../Inputs/MachineTypeDropdownInput.vue';
 import TechnologyDropdownInput from '../Inputs/TechnologyDropdownInput.vue';
 import OverlayBase from './OverlayBase.vue';
 import InputOptionList from '../BaseInputs/InputOptionList.vue';
+import OverlayButton from '../Buttons/OverlayButton.vue';
+
+const props = defineProps({
+    isCreate: {
+        type: Boolean,
+        default: true
+    }
+})
 
 const opened = ref(true)
 const breite = ref(0)
@@ -18,6 +27,18 @@ const machine_type = ref("")
 
 const technology_value = ref("")
 const set_technologies = ref([]);
+
+const button_text = props.isCreate ? "Erstellen" : "Bearbeiten";
+const technologyStore = useTechnologiesStore();
+const expand = computed(() => {
+    let num_1 = areOptionsShown.value ? technologyStore.technologies.length : 0;
+    let num_2 = set_technologies.value.length
+
+    return (num_1 + num_2) > 6;
+})
+
+const areOptionsShown = ref(false);
+
 const set_tech = (tech) => {
     if (set_technologies.value.includes(tech)) {
         return
@@ -36,50 +57,66 @@ const del_tech = (tech) => {
 <template>
     <OverlayBase title="Maschine erstellen">
         <div class="overlay-content" v-show="opened">
-            <div class="input-split">
-                <div class="form-part">
+            <div class="overlay-content-column first-column">
+                <div class="overlay-input-group">
                     <h2>Maße</h2>
-                    <NumberInput 
-                        name="Breite"
-                        id="machine-breite"
-                        v-model:value="breite" />
-                    <NumberInput 
-                        name="Länge"
-                        id="machine-laenge"
-                        v-model:value="laenge" />
+                    <div :class="{ 'split': expand }">
+                        <div class="scrollbar-padding">
+                            <NumberInput 
+                                name="Breite"
+                                id="machine-breite"
+                                v-model:value="breite" />
+                        </div>
+                        <div class="scrollbar-padding">
+                            <NumberInput 
+                                name="Länge"
+                                id="machine-laenge"
+                                v-model:value="laenge" />
+                        </div>
+                    </div>
                 </div>
-                <div class="form-part">
-                    <h2>Technologie</h2>
-                    <TechnologyDropdownInput 
-                        v-model:value="technology_value"
-                        :is-changeable="true"
-                        :is-deletable="true"
-                        :is-multi-input="true"
-                        :has-label="false"
-                        @set="set_tech" />
-                    <InputOptionList 
-                        base-id="chosen-tech-"
-                        :deleteable="true"
-                        :filtered-options="set_technologies"
-                        @delete="del_tech" />
+                <div class="overlay-input-group overlay-input-tech">
+                    <h2>Technologien</h2>
+                    <div :class="{ 'split': expand }">
+                        <InputOptionList 
+                            base-id="chosen-tech-"
+                            :deleteable="true"
+                            :filtered-options="set_technologies"
+                            @delete="del_tech" />
+                        <TechnologyDropdownInput 
+                            v-model:value="technology_value"
+                            v-model:options="areOptionsShown"
+                            :is-changeable="true"
+                            :is-deletable="true"
+                            :is-multi-input="true"
+                            :has-label="false"
+                            @set="set_tech" />
+                    </div>
                 </div>
             </div>
-            <div class="form-part">
-                <h2>Daten</h2>
-                <NumberInput 
-                    name="Maschinenstundensatz"
-                    id="machine-hourly-rate"
-                    v-model:value="machine_hourly_cost" />
-                <NumberInput 
-                    name="Investionskosten"
-                    id="investion-cost"
-                    v-model:value="investion_cost" />
-                <NumberInput 
-                    name="Zusätzliche Maschinenzeit"
-                    id="additional-machine-time"
-                    v-model:value="additional_machine_time" />
-                <MachineTypeDropdownInput 
-                    v-model:value="machine_type" />
+            <div class="overlay-content-column second-column">
+                <div class="overlay-input-group">
+                    <h2>Daten</h2>
+                    <NumberInput 
+                        name="Maschinenstundensatz"
+                        id="machine-hourly-rate"
+                        v-model:value="machine_hourly_cost" />
+                    <NumberInput 
+                        name="Investionskosten"
+                        id="investion-cost"
+                        v-model:value="investion_cost" />
+                    <NumberInput 
+                        name="Zusätzliche Maschinenzeit"
+                        id="additional-machine-time"
+                        v-model:value="additional_machine_time" />
+                    <MachineTypeDropdownInput 
+                        v-model:value="machine_type" />
+                </div>
+                <div class="overlay-input-group overlay-input-button">
+                    <OverlayButton 
+                        @click=""
+                        :text="button_text"/>
+                </div>
             </div>
         </div>
     </OverlayBase>
@@ -91,13 +128,32 @@ const del_tech = (tech) => {
     gap: 2rem;
 }
 
-.input-split {
+.overlay-content-column {
     display: flex;
     flex-direction: column;
     gap: 2rem;
 }
 
-.form-part > * {
+.first-column {
+    padding-bottom: 4rem;
+}
+
+.second-column {
+    justify-content: space-between;
+}
+
+.overlay-input-group > *,
+.overlay-input-tech > div > * {
     margin: 0 0 0.5rem 0;
+}
+
+.split {
+    display: flex;
+    gap: 1rem;
+    justify-content: flex-start;
+}
+
+.split > .scrollbar-padding {
+    margin-right: 9px;
 }
 </style>
