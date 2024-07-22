2650093b1a6bc99e852cdeae9ba9088d860bbfdf
F. Antonio Klemm <antonio@klemm.biz>
2024-07-22 11:42:32 +0200

    Added an enum class for keeping track of the current planning state.

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -12,3 +12,23 @@ export class Tool {
     return `Tool.${this.name}`
   }
 }
+
+export class PlanningState {
+  static Areas = new PlanningState('Area')
+  static Processes = new PlanningState('Processes')
+  static Machines = new PlanningState('Machines')
+  static Configuration = new PlanningState('Configuration')
+  static Overview = new PlanningState('Overview')
+
+  constructor(name) {
+    this.name = name
+  }
+
+  toString() {
+    return `PlanningState.${this.name}`
+  }
+
+  is(state) {
+    return this.name == state.name
+  }
+}
