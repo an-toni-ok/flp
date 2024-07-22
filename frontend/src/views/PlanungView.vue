d0acf9d9ff150e65a9c0313ccc6fe012e441e37d
F. Antonio Klemm <antonio@klemm.biz>
2024-07-22 11:22:46 +0200

    Created a view displaying the active input view.

---
+++ frontend/src/views/PlanungView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { PlanningState } from '@/util';
+import AreaView from './AreaView.vue';
+import ProcessView from './ProcessView.vue';
+import MachineView from './MachineView.vue';
+import { usePlanningStore } from '@/stores/planning';
+
+const planningStore = usePlanningStore();
+</script>
+
+<template>
+    <!-- Not working! -->
+    <AreaView v-if="planningStore.isActive(PlanningState.Areas)" />
+    <ProcessView v-if="planningStore.isActive(PlanningState.Processes)" />
+    <MachineView v-if="planningStore.isActive(PlanningState.Machines)" />
+</template>
+
+<style scoped>
+</style>
