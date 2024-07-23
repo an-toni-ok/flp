01d00444d825c9be0aa0f12da1d93fa41b696faa
F. Antonio Klemm <antonio@klemm.biz>
2024-07-23 18:37:51 +0200

    Added a component for expandable table fieds and added it for the technology and machine type fields in the machine table.

--- frontend/src/components/Tables/MachineTableRow.vue
+++ frontend/src/components/Tables/MachineTableRow.vue
@@ -1,20 +1,52 @@
 <script setup>
+import { computed, ref } from 'vue';
+import IconArrowDown from '../icons/IconArrowDown.vue';
+import IconButtonTable from '../Buttons/IconButtonTable.vue';
+import ExpandableText from './ExpandableText.vue';
+
 const props = defineProps({
     machine: {
         required: true,
     },
 })
+
+// const tech_open = ref(false)
+// const show_tech_dropdown = computed(() => {
+//         return props.machine.set_technologies.join(", ").length > 30
+//     },
+// )
+
+// const formatted_techs = computed(() => {
+//         var tech_text = props.machine.set_technologies.join(", ")
+//         if (tech_open.value) {
+//             return tech_text
+//         }
+//         if (tech_text.length < 30) {
+//             return tech_text
+//         }
+//         return tech_text.slice(0, 26) + "..."
+//     },
+// )
 </script>
 
 <template>
     <tr>
-        <td>{{ machine.machine_type  }}</td>
+        <td>
+            <ExpandableText 
+                help_text="Show all technologies"
+                :text="machine.machine_type" />
+        </td>
         <td>{{ machine.machine_hourly_cost }}</td>
         <td>{{ machine.investion_cost }}</td>
         <td>{{ machine.additional_machine_time }}</td>
         <td>{{ machine.breite }}</td>
         <td>{{ machine.laenge }}</td>
-        <td>{{ machine.set_technologies }}</td>
+        <td>
+            <ExpandableText 
+                help_text="Show all technologies"
+                :text="machine.set_technologies.join(', ')" />
+        </td>
+        <!-- <td>{{ machine.set_technologies }}</td> -->
         <td class="action">
             <div class="center">
                 <slot></slot>
@@ -34,13 +66,13 @@ const props = defineProps({
     justify-content: center;
 }
 
-tr > td:first-child {
-    max-width: 60ch;
-    word-wrap: break-word;
+tr > td {
+    text-align: right;
 }
 
-tr > td:nth-child(2), 
-tr > td:nth-child(3) {
-    text-align: center;
+tr > td:nth-child(1) {
+    text-align: left;
+    max-width: 30ch;
+    word-wrap: break-word;
 }
 </style>
