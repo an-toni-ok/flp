8851d01b9b43fd9e3d02ca60578e9db7546103c6
F. Antonio Klemm <antonio@klemm.biz>
2024-07-28 18:02:31 +0200

    Drawn shapes are now saved to the display

---
+++ frontend/src/components/DrawingArea/Area.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const props = defineProps({
+    width: {
+        type: Number,
+        required: true
+    },
+    height: {
+        type: Number,
+        required: true
+    },
+    posX: {
+        type: Number,
+        required: true
+    },
+    posY: {
+        type: Number,
+        required: true
+    }
+})
+
+const area = ref(undefined)
+
+onMounted(() => {
+    area.value.style.width = props.width
+    area.value.style.height = props.height
+    area.value.style.top = props.posY + "px"
+    area.value.style.left = props.posX + "px"
+})
+</script>
+
+<template>
+    <div class="area" ref="area"></div>
+</template>
+
+<style scoped>
+.area {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100px;
+    width: 100px;
+    background-color: black;
+}
+</style>
---
+++ frontend/src/components/DrawingArea/todo.md
@@ -0,0 +1,8 @@
+# What am I trying to do?
+
+Create a drawing area in which:
+- rectangles can be drawn by clicking into the area and dragging
+- rectangles size can be changed by holding a click on a rectangles border and moving the mouse.
+- rectangles can be deleted
+- rectangles have a label with their width and height displayed
+- rectangles width and height snap to certain limits (e.g. every x pixels denoting a real world distance of 0.1m)
