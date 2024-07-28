3e8ad5859a9196853169186cc9604129032ff85b
F. Antonio Klemm <antonio@klemm.biz>
2024-07-28 18:25:27 +0200

    Extracted the shape drawn when adding a new (restricted or normal) area into a component.

--- frontend/src/components/DrawingArea.vue
+++ frontend/src/components/DrawingArea.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, toRaw } from 'vue'
 import { useToolbarStore } from '@/stores/toolbar';
-import { Tool } from '@/util';
-import AreaLabel from '@/components/DrawingArea/AreaLabel.vue';
+import { Tool, DrawingShape } from '@/util';
+
+import DrawingInput from '@/components/DrawingArea/DrawingInput.vue';
 
 const toolbarStore = useToolbarStore();
 
@@ -10,35 +11,37 @@ const mouse = ref({x: 0, y: 0})
 const start_pos = ref({x: undefined, y: undefined})
 
 const mouse_down = ref(false)
-const first_position_update = ref(false)
 const element_selected = ref(false)
 
-const rects = ref([])
-const drawing_shape = ref(undefined)
+const areas = ref([])
+const restricted_areas = ref([])
+const machines = ref([])
+
 const drawing_container = ref(undefined)
 const drawing_border_class = ref("")
-const drawing_shape_display_dimensions = ref({
-    height: "0px",
-    width: "0px"
+const drawing_shape_dimensions = ref({
+    height: 0,
+    width: 0,
+    left: 0,
+    top: 0,
+    type: undefined,
 })
 
 const offset = ref({x: 0, y: 0})
 
 const update_rect = () => {
-    let x_pos = Math.min(start_pos.value.x, mouse.value.x) + "px";
-    let y_pos = Math.min(start_pos.value.y, mouse.value.y) + "px";
-
-    let width = Math.abs(mouse.value.x - start_pos.value.x) + "px";
-    let height = Math.abs(mouse.value.y - start_pos.value.y) + "px";
-
-    if (drawing_shape.value) {
-        drawing_shape.value.style.left = x_pos;
-        drawing_shape.value.style.top = y_pos;
-        drawing_shape.value.style.width = width;
-        drawing_shape_display_dimensions.value.width = width;
-        drawing_shape.value.style.height = height;
-        drawing_shape_display_dimensions.value.height = height;
-    }
+    drawing_shape_dimensions.value.left = Math.min(
+        start_pos.value.x, mouse.value.x
+    );
+    drawing_shape_dimensions.value.top = Math.min(
+        start_pos.value.y, mouse.value.y
+    );
+    drawing_shape_dimensions.value.width = Math.abs(
+        mouse.value.x - start_pos.value.x
+    );
+    drawing_shape_dimensions.value.height = Math.abs(
+        mouse.value.y - start_pos.value.y
+    );
 }
 
 const mouse_down_handler = () => {
@@ -52,9 +55,11 @@ const mouse_down_handler = () => {
         switch (toolbarStore.activeTool) {
             case Tool.Area.name:
                 drawing_border_class.value = 'area'
+                drawing_shape_dimensions.value.type = DrawingShape.Area.name
                 break
             case Tool.RestrictedArea.name:
                 drawing_border_class.value = 'restricted-area'
+                drawing_shape_dimensions.value.type = DrawingShape.RestrictedArea.name
                 break
         }
     }
@@ -72,6 +77,23 @@ const mouse_move_handler = (event) => {
 const mouse_up_handler = () => {
     if (mouse_down.value) {
         update_rect()
+        // vue refs are not clonable
+        let current_rect = toRaw(drawing_shape_dimensions.value)
+        // Otherwise the arrays in the cloned object
+        // are just a ref a to the original object and
+        // setting one updates both.
+        switch (current_rect.type) {
+            case DrawingShape.Area.name:
+                areas.value.push(structuredClone(current_rect))
+                break;
+            case DrawingShape.RestrictedArea.name:
+                restricted_areas.value.push(structuredClone(current_rect))
+                break;
+            case DrawingShape.Machine.name:
+                machines.value.push(structuredClone(current_rect))
+                break;
+        }
+
         drawing_border_class.value = ""
         mouse_down.value = false
     }
@@ -90,35 +112,43 @@ onMounted(() => {
         @mousedown="mouse_down_handler"
         @mousemove="mouse_move_handler"
         @mouseup="mouse_up_handler"
-        ref="drawing_container" >
+        ref="drawing_container"
+        :class="{ 'raised': mouse_down }" >
         <!-- @touchstart="mouse_down_handler"
         @touchmove="update"
         @touchend="mouse_up_handler"> -->
         <!-- <div class="plan-drawing"></div> -->
-        <div 
-            v-show="mouse_down"
-            class="drawing-shape" 
-            ref="drawing_shape"
-            :class="drawing_border_class" >
-            <div class="label-container">
-                <AreaLabel 
-                    :dimension-nr="drawing_shape_dimensions.width"
-                    :other-dimension="drawing_shape_dimensions.height"
-                    direction="top" />
-                <AreaLabel 
-                    :dimension-nr="drawing_shape_dimensions.width"
-                    :other-dimension="drawing_shape_dimensions.height"
-                    direction="bottom" />
-                <AreaLabel 
-                    :dimension-nr="drawing_shape_dimensions.height"
-                    :other-dimension="drawing_shape_dimensions.width"
-                    direction="left" />
-                <AreaLabel 
-                    :dimension-nr="drawing_shape_dimensions.height"
-                    :other-dimension="drawing_shape_dimensions.width"
-                    direction="right" />
-            </div>
-        </div>
+        <svg class="plan-drawing">
+            <rect v-for="rect in areas"
+                :height="rect.height"
+                :width="rect.width"
+                :x="rect.left"
+                :y="rect.top"
+                class="svg-area"
+                fill="none"
+                @mousedown="console.log('hi')"
+                ></rect>
+            <rect v-for="rect in restricted_areas"
+                :height="rect.height"
+                :width="rect.width"
+                :x="rect.left"
+                :y="rect.top"
+                class="svg-restricted-area"
+                fill="none"
+                @click="console.log('hello')"
+                ></rect>
+            <rect v-for="rect in machines"
+                :height="rect.height"
+                :width="rect.width"
+                :x="rect.left"
+                :y="rect.top"
+                class="svg-machine"
+                fill="none"
+                ></rect>
+        </svg>
+        <DrawingInput 
+            :dimensions="drawing_shape_dimensions" 
+            :mouse_down="mouse_down" />
     </div>
 </template>
 
@@ -126,7 +156,10 @@ onMounted(() => {
 .drawing-container {
     width: 100%;
     height: 100%;
-    position: relative;
+    position: absolute;
+}
+
+.raised {
     z-index: 1;
 }
 
@@ -139,32 +172,20 @@ onMounted(() => {
     height: 100%;
     position: absolute;
     top: 0;
-    background-color: lightblue;
+    background-color: var(--color-background-drawing);
 }
 
-.drawing-shape {
-    position: absolute;
-    width: 0px;
-    height: 0px;
-    top: 0;
-    left: 0;
+.svg-area {
+    stroke: var(--color-area);
+    stroke-width: 10px;
 }
 
-.area {
-    border: 10px solid var(--color-area);
+.svg-restricted-area {
+    stroke: var(--color-restricted-area);
+    stroke-width: 10px;
 }
 
-.restricted-area {
-    border: 10px solid var(--color-restricted-area);
-}
-
-.machine {
-    background-color: var(--color-machine);
-}
-
-.label-container {
-    position: relative;
-    height: 100%;
-    width: 100%;
+.svg-machine {
+    fill: var(--color-machine);
 }
 </style>
--- frontend/src/components/DrawingArea/DrawingInput.vue
+++ frontend/src/components/DrawingArea/DrawingInput.vue
@@ -1,9 +1,74 @@
 <script setup>
+import { ref, watch } from 'vue'
+import AreaLabel from '@/components/DrawingArea/AreaLabel.vue';
+
+const props = defineProps(['dimensions', 'mouse_down'])
+
+const shape = ref(undefined)
+
+watch(
+    () => props.dimensions,
+    (newValue, _) => {
+        shape.value.style.left = newValue.left + "px";
+        shape.value.style.top = newValue.top + "px";
+        shape.value.style.width = newValue.width + "px";
+        shape.value.style.height = newValue.height + "px";
+    },
+    { deep: true }
+)
 </script>
 
 <template>
-
+    <div 
+        v-show="mouse_down"
+        class="drawing-shape" 
+        ref="shape"
+        :class="dimensions.type" >
+        <div class="label-container">
+            <AreaLabel 
+                :dimension-nr="dimensions.width"
+                :other-dimension="dimensions.height"
+                direction="top" />
+            <AreaLabel 
+                :dimension-nr="dimensions.width"
+                :other-dimension="dimensions.height"
+                direction="bottom" />
+            <AreaLabel 
+                :dimension-nr="dimensions.height"
+                :other-dimension="dimensions.width"
+                direction="left" />
+            <AreaLabel 
+                :dimension-nr="dimensions.height"
+                :other-dimension="dimensions.width"
+                direction="right" />
+        </div>
+    </div>
 </template>
 
-<style>
+<style scoped>
+.drawing-shape {
+    position: absolute;
+    width: 0px;
+    height: 0px;
+    top: 0;
+    left: 0;
+}
+
+.area {
+    border: 10px solid var(--color-area);
+}
+
+.restricted-area {
+    border: 10px solid var(--color-restricted-area);
+}
+
+.machine {
+    background-color: var(--color-machine);
+}
+
+.label-container {
+    position: relative;
+    height: 100%;
+    width: 100%;
+}
 </style>
