3cd5bc629358389d92a4e0d7d43c4040f8db31ac
F. Antonio Klemm <antonio@klemm.biz>
2024-07-30 15:23:42 +0200

    Refactored the areas, restricted areas and machines into the areas store. Removed commented out code and unnecessary variables. Refactored variable names to make them clearer.

--- frontend/src/components/DrawingArea.vue
+++ frontend/src/components/DrawingArea.vue
@@ -1,22 +1,21 @@
 <script setup>
 import { onMounted, ref, toRaw } from 'vue'
 import { useToolbarStore } from '@/stores/toolbar';
+import { useAreasStore } from '@/stores/areas';
 import { Tool, DrawingShape, DrawingState, AreaCorner, AreaBorder } from '@/util';
 
 import DrawingInput from '@/components/DrawingArea/DrawingInput.vue';
 import AreaDisplay from '@/components/DrawingArea/AreaDisplay.vue';
 
 const toolbarStore = useToolbarStore();
+const areasStore = useAreasStore()
 
 const mouse = ref({x: 0, y: 0})
-const start_pos = ref({x: undefined, y: undefined})
+const shape_start_point = ref({x: undefined, y: undefined})
 const strech_x_axis = ref(false)
-const mouse_offset_in_area = ref({x: 0, y: 0})
+const moving_mouse_offset = ref({x: 0, y: 0})
 
-const areas = ref([])
-const restricted_areas = ref([])
-const machines = ref([])
-
+const drawing_area_offset = ref({x: 0, y: 0})
 const drawing_state = ref(DrawingState.Waiting.name)
 const drawing_container = ref(undefined)
 const drawing_border_class = ref("")
@@ -28,23 +27,21 @@ const drawing_shape_dimensions = ref({
     type: undefined,
 })
 
-const offset = ref({x: 0, y: 0})
-
 /**
  * Updates the drawing shape based on the mouse position.
  */
 const update_rect = () => {
     drawing_shape_dimensions.value.left = Math.min(
-        start_pos.value.x, mouse.value.x
+        shape_start_point.value.x, mouse.value.x
     );
     drawing_shape_dimensions.value.top = Math.min(
-        start_pos.value.y, mouse.value.y
+        shape_start_point.value.y, mouse.value.y
     );
     drawing_shape_dimensions.value.width = Math.abs(
-        mouse.value.x - start_pos.value.x
+        mouse.value.x - shape_start_point.value.x
     );
     drawing_shape_dimensions.value.height = Math.abs(
-        mouse.value.y - start_pos.value.y
+        mouse.value.y - shape_start_point.value.y
     );
 }
 
@@ -55,17 +52,17 @@ const update_rect = () => {
 const strech_rect = () => {
     if (strech_x_axis.value) {
         drawing_shape_dimensions.value.left = Math.min(
-            start_pos.value.x, mouse.value.x
+            shape_start_point.value.x, mouse.value.x
         );
         drawing_shape_dimensions.value.width = Math.abs(
-            mouse.value.x - start_pos.value.x
+            mouse.value.x - shape_start_point.value.x
         );
     } else {
         drawing_shape_dimensions.value.height = Math.abs(
-            mouse.value.y - start_pos.value.y
+            mouse.value.y - shape_start_point.value.y
         );
         drawing_shape_dimensions.value.top = Math.min(
-            start_pos.value.y, mouse.value.y
+            shape_start_point.value.y, mouse.value.y
         );
     }
 }
@@ -75,8 +72,8 @@ const strech_rect = () => {
  * while taking the offset of the mouse into consideration.
  */
 const move_rect = () => {
-    drawing_shape_dimensions.value.top = mouse.value.y - mouse_offset_in_area.value.y;
-    drawing_shape_dimensions.value.left = mouse.value.x - mouse_offset_in_area.value.x;
+    drawing_shape_dimensions.value.top = mouse.value.y - moving_mouse_offset.value.y;
+    drawing_shape_dimensions.value.left = mouse.value.x - moving_mouse_offset.value.x;
 }
 
 /**
@@ -85,28 +82,15 @@ const move_rect = () => {
 const storeCurrentShape = () => {
     // vue refs are not clonable
     let current_rect = toRaw(drawing_shape_dimensions.value)
-    // Otherwise the arrays in the cloned object
-    // are just a ref a to the original object and
-    // setting one updates both.
-    switch (current_rect.type) {
-        case DrawingShape.Area.name:
-            areas.value.push(structuredClone(current_rect))
-            break;
-        case DrawingShape.RestrictedArea.name:
-            restricted_areas.value.push(structuredClone(current_rect))
-            break;
-        case DrawingShape.Machine.name:
-            machines.value.push(structuredClone(current_rect))
-            break;
-    }
+    areasStore.addShape(current_rect);
 }
 
 /**
  * Creates a new shape.
  */
 const create_shape_handler = () => {
-    start_pos.value.x = mouse.value.x
-    start_pos.value.y = mouse.value.y
+    shape_start_point.value.x = mouse.value.x
+    shape_start_point.value.y = mouse.value.y
     drawing_state.value = DrawingState.Drawing.name
     update_rect()
     switch (toolbarStore.activeTool) {
@@ -128,8 +112,8 @@ const create_shape_handler = () => {
  * @param {*} event The mouse move event
  */
 const mouse_move_handler = (event) => {
-    mouse.value.x = event.pageX - offset.value.x
-    mouse.value.y = event.pageY - offset.value.y
+    mouse.value.x = event.pageX - drawing_area_offset.value.x
+    mouse.value.y = event.pageY - drawing_area_offset.value.y
     if (drawing_state.value == DrawingState.Waiting.name) {
         return;
     }
@@ -169,8 +153,8 @@ const mouse_up_handler = () => {
             break;
         case DrawingState.Move.name:
             storeCurrentShape();
-            mouse_offset_in_area.value.x = 0
-            mouse_offset_in_area.value.y = 0
+            moving_mouse_offset.value.x = 0
+            moving_mouse_offset.value.y = 0
             break;
         case DrawingState.Resize.name:
             storeCurrentShape();
@@ -191,17 +175,7 @@ const mouse_up_handler = () => {
  * @param {*} shape The shape itself
  */
 const extract_drawing_shape_from_array = (index, shape) => {
-    switch (shape.type) {
-        case DrawingShape.Area.name:
-            areas.value.splice(index, 1)
-            break
-        case DrawingShape.RestrictedArea.name:
-            restricted_areas.value.splice(index, 1)
-            break
-        case DrawingShape.Machine.name:
-            machines.value.splice(index, 1)
-            break
-    }
+    areasStore.delShape(index, shape)
     drawing_shape_dimensions.value = shape
     drawing_border_class.value = drawing_shape_dimensions.value.type;
     drawing_state.value = DrawingState.Selected.name
@@ -218,8 +192,8 @@ const move_handler = (index, shape) => {
 
     // Set the offset of the mouse click for the position 
     // calculation later.
-    mouse_offset_in_area.value.x = mouse.value.x - shape.left;
-    mouse_offset_in_area.value.y = mouse.value.y - shape.top;
+    moving_mouse_offset.value.x = mouse.value.x - shape.left;
+    moving_mouse_offset.value.y = mouse.value.y - shape.top;
 
     // Set the drawing state
     drawing_state.value = DrawingState.Move.name;
@@ -239,20 +213,20 @@ const resize_handler = (index, shape, position) => {
     // (position = cursor position)
     switch (position) {
         case AreaCorner.TopLeft:
-            start_pos.value.y = shape.top + shape.height;
-            start_pos.value.x = shape.left + shape.width;
+            shape_start_point.value.y = shape.top + shape.height;
+            shape_start_point.value.x = shape.left + shape.width;
             break;
         case AreaCorner.TopRight:
-            start_pos.value.y = shape.top + shape.height;
-            start_pos.value.x = shape.left;
+            shape_start_point.value.y = shape.top + shape.height;
+            shape_start_point.value.x = shape.left;
             break;
         case AreaCorner.BottomLeft:
-            start_pos.value.y = shape.top;
-            start_pos.value.x = shape.left + shape.width;
+            shape_start_point.value.y = shape.top;
+            shape_start_point.value.x = shape.left + shape.width;
             break;
         case AreaCorner.BottomRight:
-            start_pos.value.y = shape.top;
-            start_pos.value.x = shape.left;
+            shape_start_point.value.y = shape.top;
+            shape_start_point.value.x = shape.left;
             break;
     }
 
@@ -275,13 +249,13 @@ const stretch_handler = (index, shape, position) => {
     switch (position) {
         case AreaBorder.Top:
         case AreaBorder.Left:
-            start_pos.value.y = shape.top + shape.height;
-            start_pos.value.x = shape.left + shape.width;
+            shape_start_point.value.y = shape.top + shape.height;
+            shape_start_point.value.x = shape.left + shape.width;
             break;
         case AreaBorder.Right:
         case AreaBorder.Bottom:
-            start_pos.value.y = shape.top;
-            start_pos.value.x = shape.left;
+            shape_start_point.value.y = shape.top;
+            shape_start_point.value.x = shape.left;
             break;
     }
 
@@ -297,8 +271,8 @@ const stretch_handler = (index, shape, position) => {
 
 onMounted(() => {
     let container_offset = drawing_container.value.getBoundingClientRect()
-    offset.value.x = container_offset.left;
-    offset.value.y = container_offset.top;
+    drawing_area_offset.value.x = container_offset.left;
+    drawing_area_offset.value.y = container_offset.top;
 })
 </script>
 
@@ -310,28 +284,25 @@ onMounted(() => {
         @mouseup="mouse_up_handler"
         ref="drawing_container" 
         :class="{ 'raised': drawing_state != DrawingState.Waiting.name }">
-        <!-- :class="{ 'raised': mouse_down }" > -->
         <!-- @touchstart="create_shape_handler"
         @touchmove="update"
         @touchend="mouse_up_handler"> -->
         <!-- <div class="plan-drawing"></div> -->
         <svg class="plan-drawing">
             <AreaDisplay 
-                v-for="(area, index) in areas"
+                v-for="(area, index) in areasStore.areas"
                 :rect="area"
                 @resize="(position) => resize_handler(index, area, position)"
                 @strech="(position) => stretch_handler(index, area, position)"
                 @move="move_handler(index, area)" />
             <AreaDisplay 
-                v-model="restricted_areas" 
-                v-for="(area, index) in restricted_areas"
+                v-for="(area, index) in areasStore.restricted_areas"
                 :rect="area"
                 @resize="(position) => resize_handler(index, area, position)"
                 @strech="(position) => stretch_handler(index, area, position)"
                 @move="move_handler(index, area)" />
             <AreaDisplay 
-                v-model="machines" 
-                v-for="(machine, index) in machines"
+                v-for="(machine, index) in areasStore.machines"
                 :rect="machine"
                 @move="move_handler(index, machine)" />
         </svg>
@@ -363,18 +334,4 @@ onMounted(() => {
     top: 0;
     background-color: var(--color-background-drawing);
 }
-/* 
-.svg-area {
-    stroke: var(--color-area);
-    stroke-width: 10px;
-}
-
-.svg-restricted-area {
-    stroke: var(--color-restricted-area);
-    stroke-width: 10px;
-}
-
-.svg-machine {
-    fill: var(--color-machine);
-} */
 </style>
--- frontend/src/stores/areas.js
+++ frontend/src/stores/areas.js
@@ -1,10 +1,43 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { DrawingShape } from '@/util'
 
 export const useAreasStore = defineStore('areas', () => {
   const areas = ref([])
-  function add(restricted = false) {}
-  function remove() {}
+  const restricted_areas = ref([])
+  const machines = ref([])
 
-  return { areas, add, remove }
+  function addShape(rect) {
+    // StructuredClone needs to be used,
+    // otherwise the arrays in the cloned object
+    // are just a ref a to the original object and
+    // setting one updates both.
+    switch (rect.type) {
+      case DrawingShape.Area.name:
+        areas.value.push(structuredClone(rect))
+        break
+      case DrawingShape.RestrictedArea.name:
+        restricted_areas.value.push(structuredClone(rect))
+        break
+      case DrawingShape.Machine.name:
+        machines.value.push(structuredClone(rect))
+        break
+    }
+  }
+
+  function delShape(index, rect) {
+    switch (rect.type) {
+      case DrawingShape.Area.name:
+        areas.value.splice(index, 1)
+        break
+      case DrawingShape.RestrictedArea.name:
+        restricted_areas.value.splice(index, 1)
+        break
+      case DrawingShape.Machine.name:
+        machines.value.splice(index, 1)
+        break
+    }
+  }
+
+  return { areas, restricted_areas, machines, addShape, delShape }
 })
