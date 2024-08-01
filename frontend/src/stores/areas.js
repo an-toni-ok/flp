b0838ce6d5824e61597c76e045b065fd3dd01336
F. Antonio Klemm <antonio@klemm.biz>
2024-08-01 14:49:06 +0200

    The dimensions of the drawing are now tracked.

--- frontend/src/stores/areas.js
+++ frontend/src/stores/areas.js
@@ -8,12 +8,28 @@ export const useAreasStore = defineStore('areas', () => {
   const restricted_areas = ref([])
   const machines = ref([])
 
+  const drawing_dimensions = ref({
+    x_start: 0,
+    x_end: 0,
+    y_start: 0,
+    y_end: 0
+  })
+
   const square_dimension = computed(() => {
     const toolbarStore = useToolbarStore()
     return parseInt((20 * toolbarStore.zoom) / 100)
   })
 
   function addShape(rect) {
+    // Keep track of the dimensions in order to fit the drawing
+    // with autozoom later.
+    if (drawing_dimensions.value.x_end < rect.left + rect.width) {
+      drawing_dimensions.value.x_end = rect.left + rect.width
+    }
+    if (drawing_dimensions.value.y_end < rect.top + rect.height) {
+      drawing_dimensions.value.y_end = rect.top + rect.height
+    }
+
     // StructuredClone needs to be used,
     // otherwise the arrays in the cloned object
     // are just a ref a to the original object and
@@ -50,6 +66,7 @@ export const useAreasStore = defineStore('areas', () => {
     restricted_areas,
     machines,
     square_dimension,
+    drawing_dimensions,
     addShape,
     delShape
   }
