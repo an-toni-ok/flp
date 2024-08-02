cd5ff123ab346abea556f3031dcb6db7ffc6cab3
F. Antonio Klemm <antonio@klemm.biz>
2024-08-02 03:41:38 +0200

    Added a skeleton task to the celery shared tasks.

--- backend/server/celery_tasks.py
+++ backend/server/celery_tasks.py
@@ -3,3 +3,7 @@ from celery import shared_task
 @shared_task(ignore_result=False)
 def add_together(a: int, b: int) -> int:
     return a + b
+
+@shared_task(ignore_result=False)
+def start_optimization(run_id) -> int:
+    return True
