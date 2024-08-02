8c849f31d7e278596782fac546ddac142befad9d
F. Antonio Klemm <antonio@klemm.biz>
2024-08-02 02:19:57 +0200

    Removed an unneeded import.

--- frontend/src/components/DrawingArea/MachineDisplay.vue
+++ frontend/src/components/DrawingArea/MachineDisplay.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { computed } from 'vue';
 import { useToolbarStore } from '@/stores/toolbar';
-import { AreaBorder, AreaCorner } from '@/util';
 
 const props = defineProps({
     rect: {
