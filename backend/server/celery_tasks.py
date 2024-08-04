738b163e64ce8d760aa959a2c2816208513d8e7f
F. Antonio Klemm <antonio@klemm.biz>
2024-08-04 08:00:53 +0200

    Added an initial celery task capable of starting the optimization.

--- backend/server/celery_tasks.py
+++ backend/server/celery_tasks.py
@@ -5,5 +5,25 @@ def add_together(a: int, b: int) -> int:
     return a + b
 
 @shared_task(ignore_result=False)
-def start_optimization(run_id) -> int:
+def start_optimization(run_id):
+    import json
+    from server.RedisManager import RunInput, RunManager
+    from server.optimization import run_optimization
+
+    ri = RunInput(run_id)
+    rm = RunManager(
+        user_session_id=None, 
+        run_nr=None,
+        run_id=run_id
+    )
+
+    input_file = f"{run_id}.json"
+
+    with open(input_file, "w") as file:
+        file.write(json.dumps(ri.json()))
+
+    run_optimization(input_file)
+    
+    rm.finish({}, False)
+
     return True
