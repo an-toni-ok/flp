833ed48b17e20b73c24311aeb24e98c203c931bf
F. Antonio Klemm <antonio@klemm.biz>
2024-08-25 21:54:50 +0200

    Added a general icon button that has a configurable tooltip location and width.

---
+++ frontend/src/components/library/buttons/IconButton.vue
@@ -0,0 +1,110 @@
+<script setup>
+const props = defineProps({
+    text: {
+        type: String,
+        required: true
+    },
+    tooltipPosition: {
+        type: String,
+        default: 'bottom',
+        validator: (value) => ['top', 'bottom', 'left', 'right'].includes(value)
+    },
+    wide: {
+        type: Boolean,
+        default: false,
+    }
+})
+
+defineEmits(['click']);
+</script>
+
+<template>
+    <button
+        @click="$emit('click')"
+        class="icon-button"
+        :class="[
+            'icon-button',
+            'tooltip-pos-' + tooltipPosition, 
+            'tooltip-align-' + tooltipAlignment,
+            wide ? 'icon-button-wide' : ''
+        ]"
+        :aria-label="text"
+        :data-tool-tip="text" >
+        <div class="icon-wrapper">
+            <slot></slot>
+
+        </div>
+    </button>
+</template>
+
+<style scoped>
+/** 
+The base styling for HTML components like 
+buttons is in src/assets/main.css. 
+*/
+.icon-button {
+    position: relative;
+    display: flex;
+    background-color: var(--color-background);
+    align-items: center;
+    justify-content: center;
+    width: 2.5rem;
+    height: 2.5rem;
+    padding: 0.75rem;
+}
+
+.icon-wrapper {
+    width: 1.125rem;
+    height: 1.125rem;
+}
+
+.icon-wrapper > * {
+    width: 1.125rem;
+    height: 1.125rem;
+}
+
+.icon-button-wide {
+    width: 8.5rem;
+}
+
+.icon-button-wide > .icon-wrapper {
+    width: 7rem;
+    display: flex;
+    justify-content: flex-end;
+}
+
+.icon-button:focus {
+    outline: none;
+    border-color: var(--color-text-primary);
+    color: var(--color-text-primary);
+    z-index: 1;
+}
+
+.icon-button::after {
+    content: attr(data-tool-tip);
+    position: absolute;
+    background-color: var(--color-background);
+    padding: 0.5rem;
+    border: 1px solid var(--color-border);
+    white-space: nowrap;
+    transform: scale(0);
+    z-index: 2;
+}
+
+.icon-button:hover::after {
+    transform: scale(1);
+}
+
+.tooltip-pos-top::after {
+    bottom: calc(100% + 0.5rem);
+}
+.tooltip-pos-bottom::after {
+    top: calc(100% + 0.5rem);
+}
+.tooltip-pos-left::after {
+    right: calc(100% + 0.5rem);
+}
+.tooltip-pos-right::after {
+    left: calc(100% + 0.5rem);
+}
+</style>
