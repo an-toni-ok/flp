e7afc314796ab3f6382c5f0dd90fde14b742bff9
F. Antonio Klemm <antonio@klemm.biz>
2024-08-26 01:46:55 +0200

    Added a reusable Scrollbar that can be wrapped areound another component.

---
+++ frontend/src/components/library/styling/Scrollbar.vue
@@ -0,0 +1,44 @@
+<!-- 
+This component can be used to wrap another component
+with a scrollbar.
+
+Note:
+It will set the bottom border to have a border shown
+at the bottom regardless of the activation of the
+scrollbar.
+-->
+<script setup>
+const props = defineProps({
+    scrollStart: {
+        type: String,
+        default: "100vh"
+    }
+})
+</script>
+
+<template>
+    <div class="scrollbar" :style="{ maxHeight: scrollStart }">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.scrollbar {
+    --scrollbar-width: 6px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    width: 100%;
+    border-bottom: 1px solid var(--color-border);
+}
+
+.scrollbar::-webkit-scrollbar {
+    width: var(--scrollbar-width);
+    border-right: 1px solid var(--color-border);
+}
+
+.scrollbar::-webkit-scrollbar-thumb {
+    width: var(--scrollbar-width);
+    border-radius: 50px;
+    background-color: var(--color-border);
+}
+</style>
