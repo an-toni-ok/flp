d0cd66264f4b48dcdf31adfff699ab84995e7108
F. Antonio Klemm <antonio@klemm.biz>
2024-08-25 22:00:22 +0200

    Added a button with text.

---
+++ frontend/src/components/library/buttons/TextButton.vue
@@ -0,0 +1,47 @@
+<script setup>
+const props = defineProps({
+    text: {
+        type: String,
+        required: true
+    }
+})
+
+defineEmits(['click']);
+</script>
+
+<template>
+    <button
+        @click="$emit('click')"
+        class="icon-button" >
+        {{ text }}
+    </button>
+</template>
+
+<style scoped>
+/** 
+The base styling for HTML components like 
+buttons is in src/assets/main.css. 
+*/
+.icon-button {
+    width: var(--input-width);
+    font-size: var(--font-size);
+    line-height: var(--font-size);
+    font-weight: 600;
+    padding: 0.5rem;
+    border: 2px solid var(--color-border);
+    background-color: var(--color-background);
+}
+
+.icon-button:focus,
+.icon-button:hover {
+    border: 2px solid var(--color-text-primary);
+}
+
+.icon-button:focus > * {
+    fill: var(--color-brand);
+}
+
+.icon-button > * {
+    width: 1rem;
+}
+</style>
--- frontend/src/components/library/buttons/index.js
+++ frontend/src/components/library/buttons/index.js
@@ -1,1 +1,2 @@
 export { default as IconButton } from './IconButton.vue'
+export { default as TextButton } from './TextButton.vue'
