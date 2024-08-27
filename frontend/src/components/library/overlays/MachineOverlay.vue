d66df0ce83d432e5b430af15c500607bbba2f63a
F. Antonio Klemm <antonio@klemm.biz>
2024-08-27 03:52:57 +0200

    Created the machine input overlay, commented out the export of the ProcessOverlay as it isn't yet created.

---
+++ frontend/src/components/library/overlays/MachineOverlay.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, computed } from 'vue';
+import { useMachinesStore } from '@/stores/machines';
+import { BaseOverlay } from '@/components/library/overlays';
+import { MachineForm } from '@/components/library/forms';
+
+const opened = ref(true);
+const machinesStore = useMachinesStore();
+
+const action_type = computed(() => {
+    if (machinesStore.edit_id == 0 || !!machinesStore.edit_id) {
+        return "editieren"
+    }
+    return "erstellen"
+})
+</script>
+
+<template>
+    <BaseOverlay
+        v-model:opened="machinesStore.input_overlay_opened"
+        :title="'Maschine ' + action_type">
+        <MachineForm :action_type="action_type" />
+    </BaseOverlay>
+</template>
+
+<style scoped>
+</style>
