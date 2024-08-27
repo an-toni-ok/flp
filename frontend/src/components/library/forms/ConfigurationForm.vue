c4d68a94b44c4f84f3fd8f3fa24bd03d99561d26
F. Antonio Klemm <antonio@klemm.biz>
2024-08-27 04:32:29 +0200

    Added the configuration Form to the library.

---
+++ frontend/src/components/library/forms/ConfigurationForm.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { useSettingsStore } from '@/stores/settings';
+import { CheckboxInput, NumberInput } from '@/components/library/inputs';
+
+const settingsStore = useSettingsStore();
+</script>
+
+<template>
+    <div class="inputs">
+        <div class="general-inputs">
+            <h2>Optimierungskonfiguration</h2>
+            <NumberInput 
+                id="cycle_target_time"
+                name="Zielzykluszeit in s"
+                v-model:value="settingsStore.cycle_target_time"/>
+            <NumberInput 
+                id="hourly_operator_cost"
+                name="Stündliche Werkerlöhne (&#8364;)"
+                v-model:value="settingsStore.hourly_operator_cost"/>
+        </div>
+        <div class="checkboxes">
+            <CheckboxInput 
+                id="investion" 
+                name="Investition"
+                v-model:value="settingsStore.investion" />
+            <CheckboxInput 
+                id="piece_cost" 
+                name="Stückkosten"
+                v-model:value="settingsStore.piece_cost" />
+            <CheckboxInput 
+                id="area_usage" 
+                name="Flächenverbrauch"
+                v-model:value="settingsStore.area_usage" />
+            <CheckboxInput 
+                id="worker_amount" 
+                name="Anzahl der Operatoren"
+                v-model:value="settingsStore.worker_amount" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.inputs {
+    display: flex;
+    /* flex-direction: column; */
+    /* justify-content: space-between; */
+    gap: 4rem;
+    padding: 1.5rem;
+    border: 1px solid var(--color-border);
+}
+
+.checkboxes {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: flex-end;
+}
+
+.general-inputs {
+    margin-bottom: 4px;
+}
+
+.general-inputs > h2 {
+    font-size: var(--font-size-h3);
+    margin-bottom: 1.5rem;
+}
+
+.general-inputs > *:not(h2),
+.checkboxes > *:not(h2) {
+    margin-top: 0.5rem;
+}
+</style>
--- frontend/src/components/library/forms/index.js
+++ frontend/src/components/library/forms/index.js
@@ -1,2 +1,3 @@
 export { default as MachineForm } from './MachineForm.vue'
 export { default as ProcessForm } from './ProcessForm.vue'
+export { default as ConfigurationForm } from './ConfigurationForm.vue'
