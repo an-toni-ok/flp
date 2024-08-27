8afc2c295598ecbdea872d3a2d264415c1a6c2ce
F. Antonio Klemm <antonio@klemm.biz>
2024-08-27 04:15:26 +0200

    Added the overlay for inputting processes.

---
+++ frontend/src/components/library/overlays/ProcessOverlay.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { computed } from 'vue';
+import { useProcessesStore } from '@/stores/processes';
+import { BaseOverlay } from '@/components/library/overlays';
+import { ProcessForm } from '@/components/library/forms';
+
+const processesStore = useProcessesStore();
+const action_type = computed(() => {
+    if (processesStore.edit_id == 0 || !!processesStore.edit_id) {
+        return "editieren"
+    }
+    return "erstellen"
+})
+</script>
+
+<template>
+    <BaseOverlay
+        v-model:opened="processesStore.inputOverlayOpened"
+        :title="'Prozess ' + action_type">
+        <ProcessForm :action_type="action_type" />
+    </BaseOverlay>
+</template>
+
+<style scoped>
+</style>
