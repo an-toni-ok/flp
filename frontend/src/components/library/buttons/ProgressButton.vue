63bdd5fc7e8e2b92423d020b9fea6321a3077c0a
F. Antonio Klemm <antonio@klemm.biz>
2024-08-29 01:30:18 +0200

    Added the progress buttons to the library using the libraries components.

---
+++ frontend/src/components/library/buttons/ProgressButton.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { IconArrowLeft, IconArrowRight, IconConfirm } from '@/components/icons';
+import { IconButton } from '@/components/library/buttons';
+
+const props = defineProps({
+    complete: {
+        type: Boolean,
+        default: false
+    }
+})
+
+defineEmits(['next', 'prev'])
+</script>
+
+<template>
+    <div class="progress-buttons">
+        <IconButton 
+            text="Zurück" 
+            tooltip-position="top"
+            @click="$emit('prev')">
+            <IconArrowLeft />
+        </IconButton>
+        <IconButton 
+            text="Weiter" 
+            tooltip-position="top"
+            :wide="true"
+            @click="$emit('next')">
+            <IconArrowRight v-if="!complete" />
+            <IconConfirm v-if="complete" />
+        </IconButton>
+    </div>
+</template>
+
+<style scoped>
+.progress-buttons {
+    display: flex;
+    justify-content: flex-end;
+    z-index: 3;
+}
+
+.progress-buttons > :deep(button:last-child) {
+    /* Remove double border */
+    margin-left: -1px;
+}
+</style>
--- frontend/src/components/library/buttons/index.js
+++ frontend/src/components/library/buttons/index.js
@@ -1,2 +1,3 @@
 export { default as IconButton } from './IconButton.vue'
 export { default as TextButton } from './TextButton.vue'
+export { default as ProgressButton } from './ProgressButton.vue'
