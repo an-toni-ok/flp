4dd3d6d86637a24d3b38b4f2365cd54caa6fd6a3
F. Antonio Klemm <antonio@klemm.biz>
2024-08-29 04:20:19 +0200

    Refactored the DataOverview with the library components.

--- frontend/src/components/DataOverview.vue
+++ frontend/src/components/DataOverview.vue
@@ -1,10 +1,6 @@
 <script setup>
 import { ref } from 'vue';
-
-import MachineTable from '@/components/Tables/MachineTable.vue';
-import ProcessTable from '@/components/Tables/ProcessTable.vue';
-import IconButtonDataOverview from '@/components/Buttons/IconButtonDataOverview.vue';
-import IconArrowDown from './icons/IconArrowDown.vue';
+import { MachineTable, ProcessTable, OpenableTable } from '@/components/library/tables';
 
 const machine_table_open = ref(false)
 const process_table_open = ref(false)
@@ -32,36 +28,18 @@ const toggle_process_table = () => {
 <template>
     <div class="tables">
         <h2 class="tables-heading">Datenübersicht</h2>
-        <div class="table-container">
-            <div class="table-container-header">
-                <h3 class="table-container-heading">Prozesse</h3>
-                <IconButtonDataOverview
-                    help_text="Toggle the display of the inputted process data." 
-                    @click="toggle_process_table" >
-                    <IconArrowDown />
-                </IconButtonDataOverview>
-            </div>
-            <div class="remove-border-overlap">
-                <ProcessTable 
-                    v-show="process_table_open"
-                    :short_table="true"
-                    :show_actions="false" />
-            </div>
-        </div>
-        <div class="table-container">
-            <div class="table-container-header">
-                <h3 class="table-container-heading">Maschinen</h3>
-                <IconButtonDataOverview
-                    help_text="Toggle the display of the inputted machine data." 
-                    @click="toggle_machine_table" >
-                    <IconArrowDown />
-                </IconButtonDataOverview>
-            </div>
-            <MachineTable 
-                v-show="machine_table_open"
-                :short_table="true"
-                :show_actions="false" />
-        </div>
+
+        <OpenableTable name="Prozesse" 
+            :opened="process_table_open"
+            @click="toggle_process_table">
+            <ProcessTable scroll_start="12rem" :show_actions="false" />
+        </OpenableTable>
+        
+        <OpenableTable name="Maschinen" 
+            :opened="machine_table_open"
+            @click="toggle_machine_table">
+            <MachineTable scroll_start="15rem" :show_actions="false" />
+        </OpenableTable>
     </div>
 </template>
 
@@ -77,25 +55,7 @@ const toggle_process_table = () => {
     margin-bottom: 1rem;
 }
 
-.table-container-header {
-    display: flex;
-    width: 100%;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: -1px;
-}
-
-.table-container-heading {
-    font-size: 1rem;
-    border: 1px solid var(--color-border);
-    border-right: none;
-    width: 100%;
-    height: 2.5rem;
-    line-height: 2.5rem;
-    padding-inline: 1.5rem;
-}
-
-.remove-border-overlap {
-    margin-bottom: 10px;
+.tables > :last-child {
+    margin-top: 10px;
 }
 </style>
