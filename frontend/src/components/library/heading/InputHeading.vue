ef36c64040c98dbd2a82d7ad84a76570fb262cf8
F. Antonio Klemm <antonio@klemm.biz>
2024-08-29 02:29:18 +0200

    Added a library component for the headings of the input pages.

---
+++ frontend/src/components/library/heading/InputHeading.vue
@@ -0,0 +1,48 @@
+<script setup>
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    nr: {
+        type: Number,
+        required: true
+    },
+    max: {
+        type: Number,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="input-heading">
+        <h1>{{ title}}</h1>
+        <p><span>{{ nr }}</span> von <span>{{ max }}</span></p>
+    </div>
+</template>
+
+<style scoped>
+.input-heading {
+    display: flex;
+    align-items: flex-end;
+    background-color: var(--color-background);
+    border: 1px solid var(--color-border);
+    gap: 2rem;
+    padding: var(--tool-area-padding) 1.5rem;
+    height: 2.5rem;
+    width: fit-content;
+}
+
+.input-heading > h1 {
+    line-height: 1;
+    font-size: var(--tool-area-height);
+    font-weight: 500;
+}
+
+.input-heading > p {
+    line-height: 1;
+    font-size: 0.8rem;
+    text-wrap: nowrap;
+}
+</style>
